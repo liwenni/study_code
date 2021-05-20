0fb14cddd6262a0362fbfdab4fc08620bfe12a9c
lwn <744129268@qq.com>
2021-05-20 19:43:27 +0800

    提交代码

---
+++ java0416/Main.java
@@ -0,0 +1,13 @@
+package java0416;
+
+public class Main {
+    public static void main(String[] args) {
+//        String s;
+//        System.out.println("s="+s);
+//        int x,y;
+//        x = 5 >> 2;
+//        y = x >>> 2;
+//        System.out.println(x);
+//        System.out.println(y);
+    }
+}
